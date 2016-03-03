dcd1e3f4218b8a119fef068c2993a7989fde8922
Manuel Bachmann <mbc@iot.bzh>
2016-03-03 13:48:48 +0000

    Initial commit.
    Signed-off-by: Manuel Bachmann <mbc@iot.bzh>

---
+++ base/callback_forward.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_CALLBACK_FORWARD_H_
+#define BASE_CALLBACK_FORWARD_H_
+
+namespace base {
+
+template <typename Sig>
+class Callback;
+
+// Syntactic sugar to make Callback<void(void)> easier to declare since it
+// will be used in a lot of APIs with delayed execution.
+typedef Callback<void(void)> Closure;
+
+}  // namespace base
+
+#endif  // BASE_CALLBACK_FORWARD_H_
